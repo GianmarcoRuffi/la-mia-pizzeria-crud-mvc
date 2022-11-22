835f3e8c0f93c5967417d0d8f721cf62400ebab7
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-11-22 15:58:13 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,54 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+
+ 
+    {
+        public IActionResult Index()
+        {
+            PizzaDbContext db = new PizzaDbContext();
+
+            List<Pizza> listaPizza = db.Pizzas.ToList();
+
+        return View(listaPizza);
+        }
+
+        public IActionResult Dettaglio(int Id)
+        {
+
+            PizzaDbContext db = new PizzaDbContext();
+
+            Pizza pizza = db.Pizzas.Where(item => item.Id == Id).FirstOrDefault();
+
+            return View(pizza);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            PizzaDbContext db = new PizzaDbContext();
+
+            if (!ModelState.IsValid)
+            {
+                return View("Create", pizza);
+            }
+
+            db.Pizzas.Add(pizza);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,46 @@
+﻿using la_mia_pizzeria_static.ValidationCustomClasses;
+using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "La pizza deve avere un nome")]
+        [StringLength(40, ErrorMessage = "Il nome non può avere più di 40 caratteri")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "La pizza deve avere una descrizione")]
+        [StringLength(300, ErrorMessage = "La descrizione non può avere più di 300 caratteri")]
+        [FiveWordsValidation]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Devi inserire un link all'immagine della pizza")]
+        [StringLength(500, ErrorMessage = "Il link non può avere più di 500 caratteri")]
+        public string? Image { get; set; }
+
+        [Required(ErrorMessage = "La pizza deve avere un prezzo valido")]
+        [Range(1, 500, ErrorMessage = "La pizza deve avere un prezzo valido")]
+        public double? Prezzo { get; set; }
+
+
+        public Pizza() {
+        }
+
+        public Pizza(string name, string description, string image, double prezzo) {
+
+            this.Name = name;
+            this.Description = description;
+            this.Image= image;
+            this.Prezzo= prezzo;
+}
+    }
+
+
+
+
+
+
+
+}
