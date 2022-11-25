6fe97fb4c33524b03212bc9bb6a23450668d4793
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-11-25 12:00:16 +0100

    create repository

---
+++ Models/Repositories/DbPizzeriaRepository.cs
@@ -0,0 +1,11 @@
+﻿using la_mia_pizzeria_static.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.SqlServer.Server;
+
+namespace la_mia_pizzeria_static.Models.Repositories
+{
+    public class DbPizzeriaRepository : IDbPizzeriaRepository
+
+    {
+    }
+}
---
+++ Models/Repositories/IDbPizzeriaRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace la_mia_pizzeria_static.Models.Repositories
+{
+    public interface IDbPizzeriaRepository
+    {
+
+        List<Pizza> All();
+        void Create(Pizza pizza, List<int> IngredientiSelezionati);
+        void Delete(Pizza pizza);
+        Pizza GetById(int id);
+        void Update(Pizza pizza, Pizza formData, List<int>? IngredientiSelezionati);
+    }
+}
